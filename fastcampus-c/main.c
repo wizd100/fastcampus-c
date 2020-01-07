6d3ac9f538fed4f6e4f671151b287dde95b02f63
bkh-mac <wizd100@gmail.com>
2020-01-07 18:18:41 +0900

    c언어 02 변수와 상수

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -8,8 +8,19 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//정적변수는 0으로 자동으로 값을 대입
+int c;
+
+int main(void) {
+    //변수의 초기화
+    int a = 7;
+    printf("The Number is %d \n", a);
+    
+    //변수가 초기화가 안된 것 쓰레기값이 들어감
+    int b;
+    printf("The Number is %d \n", b);
+    
+    //정적변수
+    printf("The Number is %d \n", c);
     return 0;
 }
