82d9105d33363e937d1b0024f290b736b420e556
bkh-mac <wizd100@gmail.com>
2021-10-08 08:08:47 +0900

    함수포인터

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -7,24 +7,13 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
+
+void function() {
+    printf("func");
+}
 
 int main(void) {
-    int **p = (int**)malloc(sizeof(int*) * 3);
-    for (int i = 0; i < 3; i++) {
-        *(p + i) = (int*)malloc(sizeof(int) * 3);
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            *(*(p + i) + j) = i * 3 + j;
-        }
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", *(*(p + i) + j));
-        }
-        printf("\n");
-    }
+    printf("%d\n", function);
     
     return 0;
 }
