6eb5ef96a7fe4c1f9f65de16800421264cff4274
bkh-mac <wizd100@gmail.com>
2021-10-08 07:50:54 +0900

    메모리 동적할당 변수

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -7,14 +7,15 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    int a[2][5] = {{1,2,3,4,5}, {6,7,8,9,10}};
-    int (*p)[5] = a[1];
-    int i;
-    for (i = 0; i < 5; i++) {
-        printf("%d ", p[0][i]);
-    }
+    int *a = malloc(sizeof(int));
+    printf("%d\n", a);
+    free(a);
+    a = malloc(sizeof(int));
+    printf("%d\n", a);
+    free(a);
     
     return 0;
 }
