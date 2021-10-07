92f54c34d608a4816c2ff4414aa5b6d71e842160
bkh-mac <wizd100@gmail.com>
2021-10-08 08:06:51 +0900

    동적메모리 할당 예제

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -8,13 +8,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(void) {
-    char *a = malloc(100);
-    memset(a, 'A', 100);
-    for (int i = 0; i < 100; i++) {
-        printf("%c ", a[i]);
+    int **p = (int**)malloc(sizeof(int*) * 3);
+    for (int i = 0; i < 3; i++) {
+        *(p + i) = (int*)malloc(sizeof(int) * 3);
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            *(*(p + i) + j) = i * 3 + j;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", *(*(p + i) + j));
+        }
+        printf("\n");
     }
     
     return 0;
