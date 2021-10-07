f9bd22eb7555b32f30bc4e5a205da0260f6c53a6
bkh-mac <wizd100@gmail.com>
2021-10-08 07:53:25 +0900

    memset 을 이용한 동적으로 문자열 설정

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -8,14 +8,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-    int *a = malloc(sizeof(int));
-    printf("%d\n", a);
-    free(a);
-    a = malloc(sizeof(int));
-    printf("%d\n", a);
-    free(a);
+    char *a = malloc(100);
+    memset(a, 'A', 100);
+    for (int i = 0; i < 100; i++) {
+        printf("%c ", a[i]);
+    }
     
     return 0;
 }
