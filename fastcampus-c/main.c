58e9c4513c714a0b1c3716009030bdc47319e76a
bkh-mac <wizd100@gmail.com>
2021-10-08 07:44:31 +0900

    포인터 배열 2차원 배열

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -9,11 +9,12 @@
 #include <stdio.h>
 
 int main(void) {
-    int a[5] = {1,2,3,4,5};
-    int *p = a;
-    printf("%d\n", *(p++));
-    printf("%d\n", *(++p));
-    printf("%d\n", *(p + 2));
+    int a[2][5] = {{1,2,3,4,5}, {6,7,8,9,10}};
+    int (*p)[5] = a[1];
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("%d ", p[0][i]);
+    }
     
     return 0;
 }
