afdd1f513ca03ec5f43eb6a996955eba9c7a36e9
bkh-mac <wizd100@gmail.com>
2021-10-08 07:28:45 +0900

    2차원 배열

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -8,11 +8,16 @@
 
 #include <stdio.h>
 
+int a[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
 int main(void) {
     int i, j;
     
     for (i = 0; i < 3; i++) {
-        printf(i);
+        for (j = 0; j < 3; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
     }
     
     return 0;
