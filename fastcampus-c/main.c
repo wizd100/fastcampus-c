20b700048cf4b4e3126b9afa6d704e60e5d133ae
bkh-mac <wizd100@gmail.com>
2021-10-08 09:11:30 +0900

    전처리기 헤더

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -7,31 +7,10 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct {
-    char name[20];
-    int score;
-} Student;
+#include "temp.h"
 
 int main(void) {
-    int n;
-    int sum = 0;
-    FILE *fp;
-    fp = fopen("input.txt", "r");
-    fscanf(fp, "%d", &n);
-    Student *students = (Student*)malloc(sizeof(Student) * n);
-    for (int i = 0; i < n; i++) {
-        fscanf(fp, "%s %d", &((students + i)->name), &((students + i)->score));
-        printf("이름 : %s, 성적 : %d\n", ((students + i)->name), ((students + i)->score));
-    }
-    for (int i = 0; i < n; i++) {
-        sum += (students + i)->score;
-    }
-    free(students);
-    fclose(fp);
-    printf("점수 평균 : %.2f\n", (double)sum / n);
+    printf("%d\n", add(1, 3));
     
     return 0;
 }
---
+++ fastcampus-c/temp.h
@@ -0,0 +1,11 @@
+//
+//  temp.h
+//  fastcampus-c
+//
+//  Created by kwan ho baek on 08/10/2021.
+//  Copyright © 2021 kwan ho baek. All rights reserved.
+//
+
+int add(int a, int b) {
+    return a + b;
+}
