1a6b636fb0fae953b6177fb7204b58a225c7e3ef
bkh-mac <wizd100@gmail.com>
2021-10-08 09:04:57 +0900

    파일입출력 총합 및 해제

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -26,6 +26,12 @@ int main(void) {
         fscanf(fp, "%s %d", &((students + i)->name), &((students + i)->score));
         printf("이름 : %s, 성적 : %d\n", ((students + i)->name), ((students + i)->score));
     }
+    for (int i = 0; i < n; i++) {
+        sum += (students + i)->score;
+    }
+    free(students);
+    fclose(fp);
+    printf("점수 평균 : %.2f\n", (double)sum / n);
     
     return 0;
 }
