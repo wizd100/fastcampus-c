4a05adbb3cc1fd60c65fddbc776ddfde010d3897
bkh-mac <wizd100@gmail.com>
2021-10-08 11:29:41 +0900

    스택 동적할당

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -7,38 +7,53 @@
 //
 
 #include <stdio.h>
-#define SIZE 10000
+#include <stdlib.h>
 #define INF 999999
 
-int stack[SIZE];
-int top = -1;
+typedef struct {
+    int data;
+    struct Node *next;
+} Node;
 
-void push(int data) {
-    if (top == SIZE - 1) {
-        printf("stack overflow");
-        return;
-    }
-    stack[++top] = data;
+typedef struct {
+    Node *top;
+} Stack;
+
+void push(Stack *stack, int data) {
+    Node *node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->next = stack->top;
+    stack->top = node;
 }
 
-int pop() {
-    if (top == -1) {
+int pop(Stack *stack) {
+    if (stack->top == NULL) {
         printf("stack underflow");
+        return -INF;
     }
-    return stack[top--];
+    Node *node = stack->top;
+    int data = node->data;
+    stack->top = node->next;
+    free(node);
+    return data;
 }
 
-void show() {
-    for (int i = top; i >= 0; i--) {
-        printf("%d\n", stack[i]);
+void show(Stack *stack) {
+    Node *cur = stack->top;
+    while (cur != NULL) {
+        printf("%d\n", cur->data);
+        cur = cur->next;
     }
 }
 
 int main(void) {
-    push(7);
-    push(5);
-    push(4);
-    pop();
-    show();
+    Stack s;
+    s.top = NULL;
+    push(&s, 7);
+    push(&s, 5);
+    push(&s, 4);
+    push(&s, 3);
+    pop(&s);
+    show(&s);
     return 0;
 }
