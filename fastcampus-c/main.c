549460914eff2b209eae4339e92c8644a55b15c3
bkh-mac <wizd100@gmail.com>
2021-10-08 09:43:32 +0900

    배열기반 연결리스트

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -7,10 +7,42 @@
 //
 
 #include <stdio.h>
-#include "temp.h"
+#define INF 10000
+
+int arr[INF];
+int count = 0;
+
+void addBack(int data) {
+    arr[count] = data;
+    count++;
+}
+
+void addFirst(int data) {
+    for (int i = count; i >= 1; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = data;
+    count++;
+}
+
+void show() {
+    for (int i = 0; i < count; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+void removeAt(int index) {
+    for (int i = index; i < count; i++) {
+        arr[i] = arr[i + 1];
+    }
+    count--;
+}
 
 int main(void) {
-    printf("%d\n", add(3, 5));
-    
+    addBack(5);
+    addBack(7);
+    addFirst(1);
+    removeAt(1);
+    show();
     return 0;
 }
