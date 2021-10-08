84f6fb6160e70180508ff88a1b6cc0f018c954d2
bkh-mac <wizd100@gmail.com>
2021-10-08 10:44:38 +0900

    양방향 연결리스트

--- fastcampus-c/main.c
+++ fastcampus-c/main.c
@@ -11,51 +11,57 @@
 
 typedef struct {
     int data;
+    struct Node *prev;
     struct Node *next;
 } Node;
 
 Node *head;
+Node *tail;
 
-void addFront(Node *root, int data) {
+void insert(int data) {
     Node *node = (Node*)malloc(sizeof(Node));
     node->data = data;
-    node->next = root->next;
-    root->next = node;
-}
-
-void removeFront(Node *root) {
-    Node *front = root->next;
-    root->next = front->next;
-    free(front);
-}
-
-void freeAll(Node *root) {
-    Node *cur = head->next;
-    while (cur != NULL) {
-        Node *next = cur->next;
-        free(cur);
-        cur = next;
+    Node *cur;
+    cur = head->next;
+    while (cur->data < data && cur != tail) {
+        cur = cur->next;
     }
+    Node *prev = cur->prev;
+    prev->next = node;
+    node->prev = prev;
+    cur->prev = node;
+    node->next = cur;
 }
 
-void showAll(Node *root) {
+void removeFront() {
+    Node *node = head->next;
+    head->next = node->next;
+    Node *next = node->next;
+    next->prev = head;
+    free(node);
+}
+
+void show() {
     Node *cur = head->next;
-    while (cur != NULL) {
+    while (cur != tail) {
         printf("%d ", cur->data);
-        cur = cur->next;;
+        cur = cur->next;
     }
 }
 
 int main(void) {
     head = (Node*)malloc(sizeof(Node));
-    head->next = NULL;
-    addFront(head, 2);
-    addFront(head, 3);
-    addFront(head, 4);
-    addFront(head, 5);
-    removeFront(head);
-    showAll(head);
-    freeAll(head);
-    
+    tail = (Node*)malloc(sizeof(Node));
+    head->next = tail;
+    head->prev = head;
+    tail->next = tail;
+    tail->prev = head;
+    insert(2);
+    insert(1);
+    insert(3);
+    insert(9);
+    insert(7);
+    removeFront();
+    show();
     return 0;
 }
